273c3f88ef6cd4c3c7094a1715ad84ef58758ccd
Priyansh-Kashyap <priyansh.kashyap2107@gmail.com>
2024-07-08 20:56:43 +0530

    new class added Lab 004

---
+++ src/ex_07072024/Lab001.java
@@ -0,0 +1,9 @@
+package ex_07072024;
+
+public class Lab001 {
+    int a=5, b=10;
+    public static void main(String args[])
+    {
+        System.out.println("Hello Pramod Sir!!");
+    }
+}
---
+++ src/ex_07072024/Lab002.java
@@ -0,0 +1,8 @@
+package ex_07072024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int x=2,y=5;
+        System.out.println(x*y);
+    }
+}
