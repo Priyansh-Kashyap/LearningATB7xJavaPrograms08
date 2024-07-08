489bbc86009a839436c1753b5cf4f6e31048f64b
Priyansh-Kashyap <priyansh.kashyap2107@gmail.com>
2024-07-08 22:01:41 +0530

    This is Greater of three numbers program

---
+++ src/ex_07072024/Lab003_Greater3No.java
@@ -0,0 +1,25 @@
+package ex_07072024;
+
+class Lab003_Greater3No
+{
+
+    public static void main(String[] args)
+    {
+        int a=20, b=120, c=90;
+
+        if(a>b && a>c)
+        {
+            System.out.println("a is greater");
+
+        }else if(b>a && b>c)
+        {
+            System.out.println("b is greater");
+
+        }else
+        {
+            System.out.println("c is greater");
+        }
+
+    }
+
+}
