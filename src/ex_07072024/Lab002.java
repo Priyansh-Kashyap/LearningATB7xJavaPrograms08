c7c36a767a1b89d0b3f62b5747ebe08cb6d27bb6
Priyansh-Kashyap <priyansh.kashyap2107@gmail.com>
2024-07-08 22:00:32 +0530

    This is arithmatic operation program

--- src/ex_07072024/Lab002.java
+++
@@ -1,8 +0,0 @@
-package ex_07072024;
-
-public class Lab002 {
-    public static void main(String[] args) {
-        int x=2,y=5;
-        System.out.println(x*y);
-    }
-}
---
+++ src/ex_07072024/Lab002_ArithmaticOpt.java
@@ -0,0 +1,13 @@
+package ex_07072024;
+
+public class Lab002_ArithmaticOpt {
+    public static void main(String[] args) { //main+tab
+        int x=2,y=5;
+        System.out.println(x*y);
+        System.out.println(x+y);//sout+tab
+        System.out.println(x-y);
+        System.out.println(x%y);
+        System.out.println(x/y);
+
+    }
+}
