fc38b0c1dca66e5d2af939bc62dd9c3df757b4ca
Priyansh-Kashyap <priyansh.kashyap2107@gmail.com>
2024-07-20 14:25:29 +0530

    Triangle classifier for Equilateral, isosceles and scalene triangle

--- src/Lab006_TriangleClassisfierAssign.java
+++ src/Lab006_TriangleClassisfierAssign.java
@@ -3,19 +3,6 @@ import java.util.Scanner;
 public class Lab006_TriangleClassisfierAssign {
     public static void main(String[] args) {
 
-        //// Write a program that classifies a triangle based on its side lengths.
-        //
-        //// Given three input values representing the lengths of the sides, determine
-        //
-        //
-        //
-        //// if the triangle is equilateral (all sides are equal),
-        //
-        //// isosceles (exactly two sides are equal), or
-        //
-        //// scalene (no sides are equal).
-        //
-        //// Use an if-else statement to classify the triangle.
 
         Scanner sc = new Scanner(System.in);
 
