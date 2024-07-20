dc350fa4f50b6bdc66dcd5dcd1b1f56031bc34cf
Priyansh-Kashyap <priyansh.kashyap2107@gmail.com>
2024-07-20 14:45:39 +0530

    FizzBuzz program

---
+++ src/ex_07072024/Lab_007_FizzBuzz.java
@@ -0,0 +1,24 @@
+package ex_07072024;
+
+import java.util.Scanner;
+
+public class Lab_007_FizzBuzz {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number");
+
+        int num = sc.nextInt();
+
+        if(num%3==0 && num%5==0)
+        {
+            System.out.println("FizzBuzz");
+        } else if (num%5==0)
+        {
+            System.out.println("Buzz");
+        } else if (num%3==0 )
+        {
+            System.out.println("Fizz");
+        }
+    }
+}
