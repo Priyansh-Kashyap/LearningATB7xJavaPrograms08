9d17cd1bad72119df6d665140a924eb0d2b178c6
Priyansh-Kashyap <priyansh.kashyap2107@gmail.com>
2024-07-27 23:45:19 +0530

    This is Left Triangle Star Pattern

--- src/ex_07072024/Lab009_LeftTriStarPattern.java
+++ src/ex_07072024/Lab009_LeftTriStarPattern.java
@@ -1,8 +1,8 @@
 package ex_07072024;
 
-public class Lab009_StarPattern1 {
+public class Lab009_LeftTriStarPattern {
     public static void main(String[] args) {
-            int k=4;
+            int k=5;
         for (int i = k; i >= 1; i--) {
             for(int j =1; j<=i; j++) {
 
